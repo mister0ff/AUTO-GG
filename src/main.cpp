f61dfa3f3595c386918908f7ba9e61d03100bf4e
GameParrot <85067619+GameParrot@users.noreply.github.com>
2025-08-07 15:31:05 -0400

    1.21.100

--- src/main.cpp
+++ src/main.cpp
@@ -49,8 +49,8 @@ extern "C" [[gnu::visibility("default")]] void mod_init() {
     auto Level_typeinfo_name = hat::find_pattern(range1, hat::object_to_signature("16MultiPlayerLevel")).get();
     auto Level_typeinfo      = hat::find_pattern(range2, hat::object_to_signature(Level_typeinfo_name)).get() - sizeof(void*);
     auto Level_vtable        = hat::find_pattern(range2, hat::object_to_signature(Level_typeinfo)).get() + sizeof(void*);
-    auto Level_getTime       = reinterpret_cast<int (**)(void*)>(Level_vtable) + 123;
-    auto Level_setTime       = reinterpret_cast<void (**)(void*, int)>(Level_vtable) + 124;
+    auto Level_getTime       = reinterpret_cast<int (**)(void*)>(Level_vtable) + 125;
+    auto Level_setTime       = reinterpret_cast<void (**)(void*, int)>(Level_vtable) + 126;
 
     static auto Level_getTime_orig = *Level_getTime;
     static auto Level_setTime_orig = *Level_setTime;
