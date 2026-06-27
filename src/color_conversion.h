e548409de641cd094823f58bdfab37510e09490c
Eduhh <frazaoe620@gmail.com>
2026-06-26 21:59:16 -0300

    Delete src/color_conversion.h

--- src/color_conversion.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void RGBtoHSV(double fR, double fG, double fB, double& fH, double& fS, double& fV);
-void HSVtoRGB(double& fR, double& fG, double& fB, double fH, double fS, double fV);
